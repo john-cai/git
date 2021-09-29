4e0f05ba38754550b26c446ef960a388fe7e777a
John Cai <jcai@gitlab.com>
2021-09-29 17:08:47 -0400

    psuh: print the current branch

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,10 +1,12 @@
 #include "builtin.h"
 #include "config.h"
+#include "wt-status.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
 	int i;
 	const char *cfg_name;
+	struct wt_status status;
 
 	printf(_("Pony saying hello!. do ponies say hello?\n"));
 
@@ -21,6 +23,11 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
 		printf(_("No name is found in config\n"));
 	else
 		printf(_("Your name: %s\n"), cfg_name);
+
+	wt_status_prepare(the_repository, &status);
+	git_config(git_default_config, &status);
+
+	printf(_("Your current branch: %s\n"), status.branch);
 
 	return 0;
 }
