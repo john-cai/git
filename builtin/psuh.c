2a5679f23dfe8788814892274b106ac76f69640f
John Cai <jcai@gitlab.com>
2021-10-16 13:14:46 -0400

    psuh: usage text

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -3,6 +3,12 @@
 #include "wt-status.h"
 #include "commit.h"
 #include "pretty.h"
+#include "parse-options.h"
+
+static const char * const psuh_usage[] = {
+	N_("git psuh [<arg>...]"),
+	NULL,
+};
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
@@ -11,6 +17,12 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
 	struct wt_status status;
 	struct commit *c = NULL;
 	struct strbuf commitline = STRBUF_INIT;
+
+	struct option options[] = {
+		OPT_END()
+	};
+
+	argc = parse_options(argc, argv, prefix, options, psuh_usage, 0);
 
 	printf(_("Pony saying hello!. do ponies say hello?\n"));
 
